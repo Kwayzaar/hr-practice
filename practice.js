80baf2f93b06f91d12fe85977090b91c26a70ff9
eric <lee.eric.03@gmail.com>
2021-09-20 10:12:17 -0600

    first commit

---
+++ practice.js
@@ -0,0 +1,32 @@
+// HackerRank Practice
+
+// DAY 1 \\
+
+// 1
+// arr = [-4, 3, -9, 0, 4, 1]
+
+// function plusMinus(arr) {
+//   const negative = []
+//   const positive = []
+//   const zero = []
+  
+//   for(let i = 0;i < arr.length; i++) {
+//     if(1 / arr[i] < 0) {
+//       negative.push(arr[i])
+//     } else if (arr[i] / 1 == 0 ) {
+//       zero.push(arr[i])
+//       // return (arr.length / zero.length).toFixed(6)
+//     } else {
+//       positive.push(arr[i])
+//       // return (arr.length / positive.length).toFixed(6)
+//     }
+//   }
+//   console.log((positive.length / arr.length).toFixed(6))
+//   console.log((negative.length / arr.length).toFixed(6))
+//   console.log((zero.length / arr.length).toFixed(6))
+  
+// }
+
+// plusMinus(arr)
+
+// 2
